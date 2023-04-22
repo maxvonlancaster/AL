94dce47e0a083e947b03516815d762d0552a2c9a
maxvonlancaster <windchange7@gmail.com>
2023-04-23 00:33:26 +0300

    some changes

---
+++ AL/Utils/Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AL.Utils
+{
+    public static class Commands
+    {
+        public static Hashtable CommandHashTable = new Hashtable() 
+        {
+            {"features", () => { } }
+        };
+    }
+}
