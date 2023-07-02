0b7113672de96e6f85c985e8ad58bbf4a59f1388
maxvonlancaster <windchange7@gmail.com>
2023-07-02 21:55:00 +0300

    arduino started

--- AL/Program.cs
+++ AL/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AL;
+using AL.Services;
+
+var service = new ArduinoService();
+service.Main();
 
 var runner = new Runner();
 runner.Main();
---
+++ AL/Services/ArduinoService.cs
@@ -0,0 +1,27 @@
+﻿using NAudio.MediaFoundation;
+using Sharer;
+
+namespace AL.Services
+{
+    public class ArduinoService
+    {
+        private bool _flag = true;
+
+        public void Main() 
+        {
+            InnitConnect();
+        }
+
+        private void InnitConnect()
+        {
+            var connection = new SharerConnection("COM3", 9600);
+            while (_flag) 
+            {
+                Thread.Sleep(1000);
+                connection.Connect();
+                var result = connection.Call("Sum", 10, 12);
+            }
+            
+        }
+    }
+}
