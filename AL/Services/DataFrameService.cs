17714c81202f9af3585e76dfacaa27b55523eab7
maxvonlancaster <windchange7@gmail.com>
2023-11-03 08:38:51 +0200

    upd

---
+++ AL/Services/DataFrameService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AL.Services
+{
+    public class DataFrameService
+    {
+    }
+
+    public class DataFrame 
+    {
+        public void Add(string column, int[] ints)
+        {
+            
+        }
+
+        public IEnumerable<DataFrameRow> Rows { get; set; }
+
+        public int GetData(string column, int row) 
+        {
+            return Rows.First(r => r.Name == column).Data[row];
+        }
+    }
+
+    public class DataFrameRow 
+    {
+        public string? Name { get; set; }
+        public Collection<int> Data { get; set; }
+    }
+}
---
+++ TestApp/Services/TestService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Services
+{
+    public class TestService
+    {
+        public void Main() 
+        {
+        
+        }
+
+        public void CheckDateComparison() 
+        {
+            if (null < DateTime.Now) 
+            {
+            
+            }
+        }
+    }
+}
